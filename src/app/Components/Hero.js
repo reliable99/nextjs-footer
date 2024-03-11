244c7195e4580cab85d8d467f4da9413f5e78883
reliable99 <abdulmuhminsodiq@gmail.com>
2024-03-11 15:49:38 +0100

    footer

---
+++ src/app/Components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='my-20'>
+        <hi className="md:text-8xl text-4xl font-bold text-align-center text-gray-900 ">
+            Responsive <span className='text-blue-700'>Footer</span> using{""}
+            <span className='text-blue-700'>NextJs</span> & {""} 
+            <span className='text-blue-700'>Tailwindcss</span>
+
+        </hi>
+    </div>
+  )
+}
+
+export default Hero
